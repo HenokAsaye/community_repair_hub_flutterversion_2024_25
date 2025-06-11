fd142c3ae52bc1b3ffb38e301239a1a58210b8e1
Hermi-git <andargiehermela1@gmail.com>
2025-06-11 16:57:13 +0300

    implementing general widget test

---
+++ frontend/test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Placeholder for widget_test.dart', (WidgetTester tester) async {
+    // This is a placeholder test to make the test suite pass.
+    // TODO: Add real widget tests here.
+    expect(1, 1);
+  });
+}
