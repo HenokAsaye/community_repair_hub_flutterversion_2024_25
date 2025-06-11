c560fc52f7bafd55927009ecf671de91f3931ebb
Hermi-git <andargiehermela1@gmail.com>
2025-06-11 16:56:07 +0300

    implementing widget test

--- frontend/test/widget/login_screen_test.dart
+++ frontend/test/widget/login_screen_test.dart
@@ -1,1 +1,9 @@
-// Login Screen Widget Tests 
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Login Screen Widget Tests', () {
+    testWidgets('Placeholder test', (WidgetTester tester) async {
+      expect(1, 1);
+    });
+  });
+}
