ba730c666f040e2356c671d04286ce319f3d6cc1
Hermi-git <andargiehermela1@gmail.com>
2025-06-11 16:49:13 +0300

    update api service provider

--- frontend/lib/core/network/api_service_provider.dart
+++ frontend/lib/core/network/api_service_provider.dart
@@ -4,7 +4,7 @@ import './api_service.dart'; // Imports ApiService from the same directory
 // This is the primary base URL for your application.
 // It's taken from one of the fallback URLs in your ApiService for now.
 // Consider moving this to a more formal configuration management system if needed.
-const String _primaryBaseUrl = 'http://192.168.1.3:5500'; 
+const String _primaryBaseUrl = 'http://192.168.100.10:5500'; 
 
 final apiServiceProvider = Provider<ApiService>((ref) {
   return ApiService(baseUrl: _primaryBaseUrl);
